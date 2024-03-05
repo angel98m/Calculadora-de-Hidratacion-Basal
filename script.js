f7e753662a66033dfc2e03e63cde3243f6585be9
angel98m <160685618+angel98m@users.noreply.github.com>
2024-03-05 03:39:55 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const calcular = document.getElementById('calcular');
 const ERROR = document.getElementById('error');
 const FLU = document.getElementById('flu');
 const MAN = document.getElementById('man');
+const MEDIO = document.getElementById ('medio');
 
 calcular.addEventListener('click', () => {
     const DATO = document.getElementById('peso').value
@@ -9,15 +10,19 @@ calcular.addEventListener('click', () => {
     if (DATO > 0){
         ERROR.style.display = 'none'
         let flujo = calcFlujo(DATO);
-        let mantenimiento = flujo * 1.5;
-        FLU.innerHTML = flujo + ' cc/hr';
-        MAN.innerHTML = 'm+m/2 ' + mantenimiento + ' cc/hr';
+        let mantenimiento = flujo / 24;
+        let medio = (flujo / 24) * 1.5;
+        FLU.innerHTML = flujo + ' cc';
+        MAN.innerHTML = mantenimiento + ' cc/hr';
+        MEDIO.innerHTML = medio + 'm+m/2'
         FLU.style.display = 'block';
         MAN.style.display = 'block';
+        MEDIO.style.display = 'block';
     } else {
         ERROR.style.display = 'block';
         FLU.style.display = 'none';
         MAN.style.display = 'none';
+        MEDIO.style.display = 'none';
     }
 })
 
@@ -26,40 +31,15 @@ function calcFlujo(peso){
     let flujo = 0;
     if (resto>20){
         let aux = resto-20;
-        flujo += aux*1;
+        flujo += aux*20;
         resto -= aux;
     } 
     if (resto>10){
         let aux = resto-10;
-        flujo += aux*2;
+        flujo += aux*50;
         resto -= aux;
     }
-    flujo += resto*4;
+    flujo += resto*100;
     return flujo;
 }
 
-function superficieCorporal(peso) {
-    let superficieCorporal;
-
-    if (peso > 30) {
-        superficieCorporal = ((peso * 4) + 7) / (peso + 90);
-        FLU.style.display = 'none';
-        MAN.style.display = 'none';
-    } else {
-        superficieCorporal = (peso / 100) * 15;
-        FLU.style.display = 'none';
-        MAN.style.display = 'none';
-    }
-
-    const volumen1500 = superficieCorporal * 1500;
-    const volumen2000 = superficieCorporal * 2000;
-
-    const resultadoFinal = (volumen1500 > volumen2000) ? volumen1500 : volumen2000;
-
-    return resultadoFinal;
-}
-
-const pesoMayor30 = 35;
-const resultado = superficieCorporal(pesoMayor30);
-
-console.log(`El volumen diario recomendado es: ${resultado} `);
